83bd18fee2284ee7d5fe2bb90c6ad98456936ae5
James Coleman <james.k.coleman@gmail.com>
2014-11-21 21:17:30 -0500

    Added more javascripts to be precompiled

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,12 @@
 Rails.application.config.assets.precompile += [
   'jquery.js',
+  'jquery_ujs.js',
+  'turbolinks.js',
   'jquery.min.js',
   'sigma.min.js',
   'sigma.fisheye.js',
+  'jquery.fancybox.pack.js',
   'sigma.parseJson.js',
   'main.js'
   ]
+
