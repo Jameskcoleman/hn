b0249d0ff1d74e42a3780d52ccb1616c4447a24d
James Coleman <james.k.coleman@gmail.com>
2014-11-24 20:31:56 -0500

    Set up paratrooper. Changed text in info page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -574,7 +574,7 @@ function nodeActive(a) {
                 $GP.info_data.html(e.join("<br/>"))
 }
     $GP.info_data.show();
-    $GP.info_p.html("Connections:");
+    $GP.info_p.html("View correspondence with:");
     $GP.info.animate({width:'show'},350);
 	$GP.info_donnees.hide();
 	$GP.info_donnees.show();
